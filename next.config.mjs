88e328500a8834d0bac2214ec49032d295841fac
Rajdeep Singh <radewala.2@gmail.com>
2024-12-02 20:03:44 +0530

    adding host name

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,13 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        hostname: "images.pexels.com"
+      }
+    ],
+  },
 };
 
 export default withMDX(config);
